e4a894845eaa573dbe0962ac9486360fc980a617
laurendigital <leannelaurenlowe@hotmail.com>
2026-09-19 17:02:20 +0100

    Remove obsolete buying condition percentage columns

---
+++ supabase/migrations/20260919171000_remove_obsolete_condition_percentages.sql
@@ -0,0 +1,2 @@
+alter table public.tenant_buying_condition_rules drop column if exists never_used_percentage;
+alter table public.tenant_buying_condition_rules drop column if exists opened_percentage;

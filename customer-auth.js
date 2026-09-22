3e1552c628d55969299ff77641922716db223e89
laurendigital <leannelaurenlowe@hotmail.com>
2026-09-22 19:07:18 +0100

    Keep customer portal hidden until data handoff completes

--- customer-auth.js
+++ customer-auth.js
@@ -18,7 +18,6 @@ function revealPortal(){
  if(portal)portal.hidden=false;
 }
 function dispatchAuthSuccess(data){
- revealPortal();
  if(typeof window.tradeflowHandleCustomerAuthSuccess==='function'){
   window.tradeflowHandleCustomerAuthSuccess(data);
  }else{
